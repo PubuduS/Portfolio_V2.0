20a320744db19e92fca41f8aca0ad6117515561d
PubuduS <pubudusupun@gmail.com>
2023-09-24 01:36:58 -0500

    Added a responsive header section.
    Installed ng-bootstrap.
    Add images and logos.
    Add a responsive header.
    Add global css styling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'mp-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio2.0';
+
+  
+  constructor(private modalService: NgbModal) {
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
+  
 }
